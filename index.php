d6d661a10ed9472c3335666a1bdce769cac020b3
Filippo <filippo.botti.itis@gmail.com>
2020-09-17 23:10:09 +0200

    Added homepage caption

--- index.php
+++ index.php
@@ -7,20 +7,24 @@ require_once 'includes/header.php';
 <div class="row justify-content-center">
 <div class="col-md-6">
         <img src="https://avatars0.githubusercontent.com/u/37662752?s=400&u=6c68df7950220928f49ee2246a2425b6a43c1cec&v=4" id="profileimg" alt="Mattia Ricci">
-
+<br>
         <p class="paragraph-text">
-            ConcertEdia è il sito che ti permette di fare un tuffo nella bella musica del passato.
-            Cerca il tuo concerto preferito, guarda il video e immergiti nella descrizione.
+            E' uno studente del terzo anno presso la facoltà Ingegneria dei Sistemi Informativi 
+            dell'Università degli studi di Parma.
+            Il concerto che più lo ha emozionato, anche se l'ha potuto solo vedere su internet,
+            è stato il Live a Wembley dei Queen, con la mitica Who Wants to Live Forever.
         </p>
     </div>
 
     <div class="col-md-6">
         <img src="https://avatars1.githubusercontent.com/u/32622328?s=400&u=dccb850e22766b7a9fab52ed04cf4e175ab6a29f&v=4" id="profileimg" alt="Filippo Botti">
-
+        <br>
         <p class="paragraph-text">
-            ConcertEdia è il sito che ti permette di fare un tuffo nella bella musica del passato.
-            Cerca il tuo concerto preferito, guarda il video e immergiti nella descrizione.
-        </p>
+        E' uno studente del terzo anno presso la facoltà Ingegneria informarica 
+        dell'Università degli studi di Parma.
+        Il concerto che più lo ha emozionato, anche se l'ha potuto solo vedere su internet,
+        è stato il Celebration Day dei Led Zeppelin, dove una fantastica interpretazione
+        di Staiway to Heaven lo ha stregato.
     </div>
 </div>
 
