f81253c364d0eb9fd6da998cea4966f1f30066f1
Filippo <filippo.botti.itis@gmail.com>
2020-09-17 15:54:34 +0200

    Fixed footer.php

--- index.php
+++ index.php
@@ -2,7 +2,28 @@
 require_once 'includes/header.php';
 ?>
 
-<h1> hi </h1>
+<h1> CONCERTEDIA </h1>
+
+<div class="row justify-content-center">
+<div class="col-md-6">
+        <img src="https://avatars0.githubusercontent.com/u/37662752?s=400&u=6c68df7950220928f49ee2246a2425b6a43c1cec&v=4" id="profileimg" alt="Mattia Ricci">
+
+        <p class="paragraph-text">
+            ConcertEdia è il sito che ti permette di fare un tuffo nella bella musica del passato.
+            Cerca il tuo concerto preferito, guarda il video e immergiti nella descrizione.
+        </p>
+    </div>
+
+    <div class="col-md-6">
+        <img src="https://avatars1.githubusercontent.com/u/32622328?s=400&u=dccb850e22766b7a9fab52ed04cf4e175ab6a29f&v=4" id="profileimg" alt="Filippo Botti">
+
+        <p class="paragraph-text">
+            ConcertEdia è il sito che ti permette di fare un tuffo nella bella musica del passato.
+            Cerca il tuo concerto preferito, guarda il video e immergiti nella descrizione.
+        </p>
+    </div>
+</div>
+
 
 <?php
 require_once 'includes/footer.php';
