ab01dc089bc54b5a057dc66d3e798a0a379ebb62
Filippo <filippo.botti.itis@gmail.com>
2020-09-19 21:51:24 +0200

    Added site description in the homepage

--- index.php
+++ index.php
@@ -5,6 +5,13 @@ require_once 'includes/header.php';
 <body>
     <div class="text-center pt-2 pb-4">
         <h1 class="widget-title"> ConcertEdia </h1>
+            <h4>ConcertEdia è tutto quello che cerchi se vuoi rivivere un concerto storico.
+                <br> Lasciati cullare dalle descrizioni dei più grandi esperti di musica
+                <br> Esplora i grandi palcoscenici della storia della musica
+                <br> Ascolta le hit del passato
+                <br>Let's rock!
+                
+            </h4>
     </div>
 
     <div class="container-fluid">
