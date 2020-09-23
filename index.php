bdc8c0535b3352e5673d1ef09f58bfd2037ac6f2
Filippo <filippo.botti.itis@gmail.com>
2020-09-23 11:08:03 +0200

    Fixed home

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ require_once 'includes/header.php';
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-md-6">
-                        <div class="col-sm-2 pb-2 inverted">
+                        <div class="contenuto-centrato pb-2">
                             <img src="https://avatars0.githubusercontent.com/u/37662752?s=400&u=6c68df7950220928f49ee2246a2425b6a43c1cec&v=4" id="profileimg" alt="Mattia Ricci">
                         </div>
                         <p class="paragraph-large pt-2">
@@ -29,7 +29,7 @@ require_once 'includes/header.php';
                         </p>
                     </div>
                     <div class="col-md-6">
-                        <div class="col-sm-2 text-center pb-2 inverted">
+                        <div class="contenuto-centrato pb-2">
                             <img src="https://avatars1.githubusercontent.com/u/32622328?s=400&u=dccb850e22766b7a9fab52ed04cf4e175ab6a29f&v=4" id="profileimg" alt="Filippo Botti">
                         </div>
                         <p class="paragraph-large pt-2">
